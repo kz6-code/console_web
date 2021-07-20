92182441eb5000382241ba388a7e7f74b26a7f61
kz6-code <80061504+kz6-code@users.noreply.github.com>
2021-07-20 05:43:38 -1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var first_name = "Ali";
+var last_name = "Muhammad";
+var add = first_name.concat(last_name);
+var a = 3;
+var b = 8;
+var number = a + b;
+console.log(add);
+console.log(number);
+
+var number_sub = a - b;
+var number_div = a / b;
+var number_mul = a * b;
+
+console.log(number_sub);
+console.log(number_div);
+console.log(number_mul);
